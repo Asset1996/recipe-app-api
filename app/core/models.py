f38ec6150a1392a4c7f8affa40af37a6bee0dd45
Asset Khaitbay <assetkhaitbay@MacBook-Air-Asset.local>
2022-02-04 15:09:23 +0500

    Added create_superuser function into UserManager class.

--- app/core/models.py
+++ app/core/models.py
@@ -5,10 +5,19 @@ class UserManager(BaseUserManager):
 
     def create_user(self, email, password=None, **extra_fields):
         """Creates and saves new user."""
+        if not email:
+            raise ValueError('Email is required!')
         user = self.model(email=self.normalize_email(email), **extra_fields)
         user.set_password(password)
         user.save(using=self._db)
+        return user
 
+    def create_superuser(self, email, password):
+        """Creates and saves new super user."""
+        user = self.create_user(email, password)
+        user.is_staff = True
+        user.is_superuser = True
+        user.save(using=self._db)
         return user
 
 
@@ -22,6 +31,3 @@ class User(AbstractBaseUser, PermissionsMixin):
     objects = UserManager()
 
     USERNAME_FIELD = 'email'
-    
-
-
--- app/core/tests/test_models.py
+++ app/core/tests/test_models.py
@@ -4,7 +4,7 @@ from django.contrib.auth import get_user_model
 class ModelTests(TestCase):
 
     def test_create_user_with_email_successful(self):
-        """Test creating a new user with email confirmation"""
+        """Test creating a new user with email."""
         email = 'test@mail.com'
         password = '1234'
         user = get_user_model().objects.create_user(
@@ -25,3 +25,20 @@ class ModelTests(TestCase):
             password = password
         )
         self.assertEqual(user.email, email.lower())
+    
+    def test_create_user_with_invalid_email(self):
+        """Create user with invalid email."""
+        with self.assertRaises(ValueError):
+            get_user_model().objects.create_user(None, '1234')
+
+    def test_create_superuser_with_email_successful(self):
+        """Test creating a new super user with email."""
+        email = 'test@mail.com'
+        password = '1234'
+        user = get_user_model().objects.create_superuser(
+            email=email,
+            password=password
+        )
+
+        self.assertTrue(user.is_staff)
+        self.assertTrue(user.is_superuser)
